9735bc6da1b821bb2e63bce02547466b8f7fd167
HanhDuyen123 <hanhduyentranthi109@gmail.com>
2025-06-15 18:43:58 +0700

    final CNPM

--- GalaxyCinema/App_Start/RouteConfig.cs
+++ GalaxyCinema/App_Start/RouteConfig.cs
@@ -17,7 +17,6 @@ namespace GalaxyCinema
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                //defaults: new { controller = "Movies", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
